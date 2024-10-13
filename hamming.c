b5d61b14741b806299c1d4b7e8be2b7016919cd9
Leo Jeong <80350771+33tm@users.noreply.github.com>
2024-10-13 13:05:08 -0700

    statically allocate fixed blocks

--- hamming.c
+++ hamming.c
@@ -12,19 +12,14 @@ const int G[7][4] = {
     { 0, 0, 0, 1 }
 };
 
-uint8_t *encodeBlock(uint8_t block[4]) {
-    uint8_t *encoded = malloc(8);
-
+void encodeBlock(uint8_t encoded[8], uint8_t block[4]) {
     for (int i = 0; i < 7; i++) {
         int dp = 0;
         for (int j = 0; j < 4; j++)
             dp += G[i][j] * block[j];
         encoded[i] = dp % 2;
     }
-
     encoded[7] = 0;
-
-    return encoded;
 }
 
 void encode(char *input_file, char *output_file) {
@@ -46,18 +41,17 @@ void encode(char *input_file, char *output_file) {
 
     FILE *output = fopen(output_file, "wb");
     for (size_t i = 0; i < size * 8; i += 4) {
-        uint8_t *block = encodeBlock(blocks + i);
-
+        uint8_t block[8];
+        encodeBlock(block, blocks + i);
+        
         uint8_t byte = 0;
         for (int j = 0; j < 7; j++)
-            byte |= (block[j] << (6 - j));
-
+            byte |= block[j] << 6 - j;
         fwrite(&byte, 1, 1, output);
-        free(block);
     }
 
-    fclose(output);
     free(blocks);
+    fclose(output);
 }
 
 const int H[3][7] = {
@@ -73,8 +67,8 @@ const int D[4][7] = {
     { 0, 0, 0, 0, 0, 0, 1 },
 };
 
-uint8_t *decodeBlock(uint8_t block[7]) {
-    uint8_t *error = malloc(3);
+void decodeBlock(uint8_t decoded[4], uint8_t block[7]) {
+    uint8_t error[3];
 
     for (int i = 0; i < 3; i++) {
         int dp = 0;
@@ -88,18 +82,12 @@ uint8_t *decodeBlock(uint8_t block[7]) {
     if (index != 0)
         block[index - 1] ^= 1;
 
-    free(error);
-
-    uint8_t *decoded = malloc(4);
-
     for (int i = 0; i < 4; i++) {
         int dp = 0;
         for (int j = 0; j < 7; j++)
             dp += D[i][j] * block[j];
         decoded[i] = dp % 2;
     }
-
-    return decoded;
 }
 
 void decode(char *input_file, char *output_file) {
@@ -121,23 +109,18 @@ void decode(char *input_file, char *output_file) {
 
     FILE *output = fopen(output_file, "wb");
     for (size_t i = 0; i < size * 7; i += 14) {
-        uint8_t *block1 = decodeBlock(blocks + i);
-        uint8_t *block2 = decodeBlock(blocks + i + 7);
+        uint8_t block[8];
+        decodeBlock(block, blocks + i);
+        decodeBlock(block + 4, blocks + i + 7);
 
         uint8_t byte = 0;
-        for (int j = 0; j < 4; j++) {
-            byte |= (block1[j] << (7 - j));
-            byte |= (block2[j] << (3 - j));
-        }
-
-        free(block1);
-        free(block2);
-
+        for (int j = 0; j < 8; j++)
+            byte |= block[j] << 7 - j;
         fwrite(&byte, 1, 1, output);
     }
 
-    fclose(output);
     free(blocks);
+    fclose(output);
 }
 
 void corrupt(char *input_file, char *output_file) {
@@ -156,8 +139,8 @@ void corrupt(char *input_file, char *output_file) {
 
     FILE *output = fopen(output_file, "wb");
     fwrite(buffer, size, 1, output);
-    fclose(output);
     free(buffer);
+    fclose(output);
 }
 
 int main(int argc, char *argv[]) {
